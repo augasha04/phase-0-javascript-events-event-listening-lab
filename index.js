93c394d7b164064f806f1b05ae8add9eb9d8040b
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-03-13 08:35:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert ( "hee hee, that tickles");
+
 }
+input.addEventListener('click', clickAlert);
